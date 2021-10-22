ecd3be2fa6b98bcbaf8373f35a9048ec9180685d
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-22 11:46:21 +0300

    настроил получение токена

--- src/modules/login/login.vue
+++ src/modules/login/login.vue
@@ -1,26 +1,57 @@
 <template src="./login.html"></template>
-<script >
-export default{
-data(){
-    return{
-        loginInput:'',
-        passwordInput:''
-
-    }
-},
-
-    methods:{
-        reg(){
-            if ((this.loginInput == '' || this.loginInput == null) || (this.passwordInput == '' || this.passwordInput == null)) {return}
-            console.log(this.loginInput)
-            console.log(this.passwordInput)
-        }
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            loginInput: '',
+            passwordInput: '',
+            token: null
+        };
     },
-}
 
+    methods: {
+        sendRegRequest() {
+            if (
+                this.loginInput == '' ||
+                this.loginInput == null ||
+                this.passwordInput == '' ||
+                this.passwordInput == null
+            ) {
+                return;
+            }
+            axios
+                .post('http://dev1.itpw.ru:8003/accounts/authentication/reg/', {
+                    username: this.loginInput,
+                    password: this.passwordInput
+                })
+                .then((response) => {
+                    console.log(response);
+                });
+        },
+        sendLoginRequest() {
+            if (
+                this.loginInput == '' ||
+                this.loginInput == null ||
+                this.passwordInput == '' ||
+                this.passwordInput == null
+            ) {
+                return;
+            }
+            axios
+                .post(
+                    'http://dev1.itpw.ru:8003/accounts/authentication/auth/',
+                    {
+                        username: this.loginInput,
+                        password: this.passwordInput
+                    }
+                )
+                .then((response) => {
+                    window.localStorage.setItem('token', response.data.token);
+                    console.log(window.localStorage.getItem('token'));
+                });
+        }
+    }
+};
 </script>
-<style src="./login.scss" lang="scss">
-
-
-
-</style>
+<style src="./login.scss" lang="scss"></style>
