5daf6ecbae36a432d1ede1e351d0c3b093bdb1e3
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-22 16:32:35 +0300

    +todo list

--- src/pages/todo/Todo.vue
+++ src/pages/todo/Todo.vue
@@ -5,7 +5,10 @@
         <div class="row mb-2">
             <div class="col-sm-6 row">
                 <h1>Todo List</h1> 
-                <button  @click="clearTodoPosts" class="clear-list_btn">Clear list</button>
+                <h6 v-if="todoPosts.length == 0"  class="completed">Завершено</h6>
+                <h6 v-if="todoPosts.length == 0"  class="completed">{{completedCount}}</h6>
+                
+                <!-- <button  @click="clearTodoPosts" class="clear-list_btn">Clear list</button> -->
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -26,8 +29,8 @@
                <button @click="pushListToTodoPosts" class='col-sm-1'>Create list</button>
               <ul class=" todo-list">
                   <li class='todo-list_item '  v-for="(post, index) in todoPosts"
-          ><span>{{post}}</span>
-                      <button @click="todoPosts.splice(index, 1)" class="col-sm-1 clear-list_item__btn">Delete</button>
+          ><span>{{post.name }}</span>
+                      <button @click="clearTodoPosts(index,post.id)" class="col-sm-1 clear-list_item__btn">Delete</button>
                   </li> 
               </ul>
             </div>
@@ -40,49 +43,100 @@
 
 </template>
 <script>
+import axios from 'axios';
 export default {
-    name: "Todo",
-    data(){
-        return{
-            inputHolder:null,
-            todoPosts : [],
+    name: 'Todo',
+    data() {
+        return {
+            inputHolder: null,
+            todoPosts: []
+        };
+    },
+    methods: {
+        pushListToTodoPosts() {
+            if (this.inputHolder === null || this.inputHolder === '') {
+                return;
+            }
+            axios
+                .post(
+                    'http://dev1.itpw.ru:8003/todo/lists/',
+                    {
+                        name: this.inputHolder
+                    },
+                    {
+                        headers: {
+                            Authorization:
+                                'Bearer ' + window.localStorage.getItem('token')
+                        }
+                    }
+                )
+                .then((response) => {
+                    this.todoPosts.push({
+                        name: response.data.name,
+                        id: response.data.id
+                    });
+                });
+            this.inputHolder = '';
+        },
+
+        clearTodoPosts(index, id) {
+            axios.delete('http://dev1.itpw.ru:8003/todo/lists/' + id, {
+                headers: {
+                    Authorization:
+                        'Bearer ' + window.localStorage.getItem('token')
+                }
+            });
+
+            this.todoPosts.splice(index, 1);
+        },
+        renderPosts() {
+            this.todoPosts = [];
+            axios
+                .get('http://dev1.itpw.ru:8003/todo/lists/', {
+                    headers: {
+                        Authorization:
+                            'Bearer ' + window.localStorage.getItem('token')
+                    }
+                })
+                .then((response) => {
+                    for (let i = 0; i < response.data.length; i++) {
+                        this.todoPosts.push({
+                            name: response.data[i].name,
+                            id: response.data[i].id
+                        });
+                        console.log(response.data);
+                    }
+                });
         }
     },
-    methods:{
-        pushListToTodoPosts(){
-            if ((this.inputHolder === null) ||(this.inputHolder === '')) { return } 
-                this.todoPosts.push(this.inputHolder)
-                this.inputHolder = ''
-            
-            
-        },
-        clearTodoPosts(){
-            this.todoPosts = []
-        },
-    },
-
-}
+    beforeMount() {
+        this.renderPosts();
+        console.log(this.todoPosts);
+    }
+};
 </script>
 <style>
-
-.todo-list li{
-    margin-top:15px ;
+.todo-list li {
+    margin-top: 15px;
     display: flex;
     width: 83%;
     justify-content: space-between;
 }
-.clear-list_btn{
-    margin-left:15px;
-    background-color: red ;
+.clear-list_btn {
+    margin-left: 15px;
+    background-color: red;
     color: white;
     border: 1px solid white;
 }
 
-.clear-list_item__btn{
-    
-    background-color: red ;
+.clear-list_item__btn {
+    background-color: red;
     color: white;
     border: 1px solid white;
 }
 
+.completed {
+    color: rgb(76, 148, 4);
+    font-weight: 700;
+}
 </style>
