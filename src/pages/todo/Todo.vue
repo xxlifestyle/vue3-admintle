6c47a3f296aa18782058af6c2bbcb70ee7a35f19
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2021-10-23 00:16:01 +0300

    Update Todo.vue

--- src/pages/todo/Todo.vue
+++ src/pages/todo/Todo.vue
@@ -6,9 +6,6 @@
             <div class="col-sm-6 row">
                 <h1>Todo List</h1> 
                 <h6 v-if="todoPosts.length == 0"  class="completed">Завершено</h6>
-                <h6 v-if="todoPosts.length == 0"  class="completed">{{completedCount}}</h6>
-                
-                <!-- <button  @click="clearTodoPosts" class="clear-list_btn">Clear list</button> -->
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -23,19 +20,26 @@
     <div class="container-fluid">
         <div class="card">
             
-           
             <div class="card-body">
-               <input @keyup.enter="pushListToTodoPosts" v-model='inputHolder' placeholder="Enter your task" class='col-sm-9'>
-               <button @click="pushListToTodoPosts" class='col-sm-1'>Create list</button>
-              <ul class=" todo-list">
-                  <li class='todo-list_item '  v-for="(post, index) in todoPosts"
-          ><span>{{post.name }}</span>
-                      <button @click="clearTodoPosts(index,post.id)" class="col-sm-1 clear-list_item__btn">Delete</button>
-                  </li> 
-              </ul>
-            </div>
+            <input @keyup.enter="pushListToTodoPosts" v-model='inputListHolder' placeholder="Enter your list" class='col-sm-9'>
+            <button @click="pushListToTodoPosts" class='col-sm-1'>Create list</button>
+            <ul class=" todo-list">
+                <li class='todo-list_item' v-for="(post, index) in todoPosts"><span>{{post.name }}</span>
+                    <button @click="clearTodoPosts(index,post.id)" class="col-sm-1 clear-list_item__btn">Delete</button>
+                    <ul v-if="todoPosts.length !== 0" class=" task-list">
+                        <li class='task-list_item '  v-for="(task, index) in taskPosts"><span>{{ task.name }}</span>
+                            <button @click="clearTodoTasks(index,task.id)" class="col-sm-1 clear-list_item__btn">Delete</button>
+                
+                
+                        </li> 
+                <input v-model='inputTaskHolder' placeholder="Enter your task" class=' task-input col-sm-2'/> 
+                <button @click="pushListToTaskPosts(post.id)" class='col-sm-1'>Create task</button>
+                    </ul>
+                </li> 
+                    
+            </ul>
+        </div>
             <div class="card-footer">Footer</div>
-           
         </div>
     </div>
 </section>
@@ -48,20 +52,59 @@ export default {
     name: 'Todo',
     data() {
         return {
-            inputHolder: null,
-            todoPosts: []
+            inputListHolder: null,
+            inputTaskHolder: null,
+            todoPosts: [],
+            taskPosts: []
         };
     },
     methods: {
+        pushListToTaskPosts(id) {
+            if (this.inputTaskHolder === null || this.inputTaskHolder === '') {
+                return;
+            }
+            axios
+                .post(
+                    'http://dev1.itpw.ru:8003/todo/tasks/',
+                    {
+                        name: this.inputTaskHolder,
+                        todo_list: id
+                    },
+                    {
+                        headers: {
+                            Authorization:
+                                'Bearer ' + window.localStorage.getItem('token')
+                        }
+                    }
+                )
+                .then((response) => {
+                    this.taskPosts.push({
+                        name: response.data.name,
+                        id: response.data.id
+                    });
+                });
+            this.inputTaskHolder = '';
+        },
+        clearTodoTasks(index, id) {
+            axios.delete('http://dev1.itpw.ru:8003/todo/tasks/' + id, {
+                headers: {
+                    Authorization:
+                        'Bearer ' + window.localStorage.getItem('token')
+                }
+            });
+
+            this.taskPosts.splice(index, 1);
+        },
+
         pushListToTodoPosts() {
-            if (this.inputHolder === null || this.inputHolder === '') {
+            if (this.inputListHolder === null || this.inputListHolder === '') {
                 return;
             }
             axios
                 .post(
                     'http://dev1.itpw.ru:8003/todo/lists/',
                     {
-                        name: this.inputHolder
+                        name: this.inputListHolder
                     },
                     {
                         headers: {
@@ -76,7 +119,7 @@ export default {
                         id: response.data.id
                     });
                 });
-            this.inputHolder = '';
+            this.inputListHolder = '';
         },
 
         clearTodoPosts(index, id) {
@@ -107,16 +150,44 @@ export default {
                         console.log(response.data);
                     }
                 });
+        },
+        renderTasks() {
+            this.todoPosts = [];
+            axios
+                .get('http://dev1.itpw.ru:8003/todo/tasks/', {
+                    headers: {
+                        Authorization:
+                            'Bearer ' + window.localStorage.getItem('token')
+                    }
+                })
+                .then((response) => {
+                    for (let i = 0; i < response.data.length; i++) {
+                        this.taskPosts.push({
+                            name: response.data[i].name,
+                            id: response.data[i].id
+                        });
+                        console.log(response.data);
+                    }
+                });
         }
     },
     beforeMount() {
         this.renderPosts();
+        this.renderTasks();
         console.log(this.todoPosts);
+        console.log(this.renderTasks);
     }
 };
 </script>
 <style>
 .todo-list li {
+    margin-top: 15px;
+    display: flex;
+    width: 83%;
+    justify-content: space-between;
+}
+
+.task-list li {
     margin-top: 15px;
     display: flex;
     width: 83%;
@@ -138,5 +209,8 @@ export default {
 .completed {
     color: rgb(76, 148, 4);
     font-weight: 700;
+}
+.task-input {
+    margin-top: 15px;
 }
 </style>
